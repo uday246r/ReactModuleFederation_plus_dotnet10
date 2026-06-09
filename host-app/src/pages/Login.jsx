396210f8baf4ca498d000db606ed3e88932b639b
Uday246r <udaychauhan246r@gamil.com>
2026-06-09 08:59:29 +0530

    useNavigate used

--- host-app/src/pages/Login.jsx
+++ host-app/src/pages/Login.jsx
@@ -9,6 +9,8 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+  
   const loginUser = async (e) => {
 
     e.preventDefault();
